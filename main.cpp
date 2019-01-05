8aab6f2bc2d8844042719297a1619281ca3421e9
Rafi Alwan S <45717061+Rafflesia3@users.noreply.github.com>
2019-01-06 00:10:21 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main()
+{
+    int bil,jum,i;
+
+    cout << "\t\t Pelita Bangsa \n\n" << endl;
+    cout << "=========================================\n" << endl;
+    cout << "Nama    : Rafi Alwan Setyawan \nNIM     : 311810325 \nKelas   : TI.18.D7 \n" << endl;
+    cout << "=========================================\n" << endl;
+
+    cout<< "\t     Menentukan bilangan prima\n" << endl;
+
+    cout<<"MASUKKAN BILANGAN BULAT POSITIF = ";
+    cin>>bil;
+    jum = 0;
+    for (i=1;i<=bil;i++)
+    if (bil%i==0)
+    jum++;
+    if (jum==2)
+        cout<<"BILANGAN TERSEBUT ADALAH BILANGAN PRIMA\n";
+    else
+        cout<<"BUKAN BILANGAN PRIMA\n";
+    return 0;
+}
